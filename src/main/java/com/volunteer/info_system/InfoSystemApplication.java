c3d14953e3ea5dcf4a3cac6c13078249c77a499f
shivanand9019 <shivanandmadar512@gmail.com>
2026-06-16 23:40:16 +0530

    Application Setup

---
+++ src/main/java/com/volunteer/info_system/InfoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.volunteer.info_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volunteer/info_system/InfoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteer.info_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
